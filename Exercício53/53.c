87ba27bce7e3412ffece7dc99fe24560a9435d73
tcbnunes <106353741+tcbnunes@users.noreply.github.com>
2022-06-10 14:28:21 -0300

    53

--- Exercício53/53.c
+++ Exercício53/53.c
@@ -10,15 +10,28 @@ a população do país B
 int main(){
 system("cls");
 //DECLARAÇÃO DE VARIAVEIS
-
+int ano = 0;
+unsigned int habitantesA, habitantesB, taxaA, taxaB;
 //ENTRADA DE VALORES
-
+habitantesA = 5000000;
+habitantesB = 7000000;
+taxaA = 3;
+taxaB = 2;
 
 //CORPO DO ALGORITIMO
-
+while (habitantesA<habitantesB)
+{
+    habitantesA = habitantesA*(taxaA+100)/100;
+    habitantesB = habitantesB*(taxaB+100)/100;
+    ano++;
+}
 
 //SAIDA DE VALORES
-printf("\n");
+if(ano==0){
+    printf("O pais A ja possui maior populacao");
+}else{
+    printf("O pais A ultrapassou a populacao do pais B em %d anos.\n");
+}
 //ENCERRAMENTO DO ALGORITIMO
 system("PAUSE");
 return 0;
