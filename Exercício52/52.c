14d5a48f601cc3be023a41cb8a38feb640f7aaed
tcbnunes <106353741+tcbnunes@users.noreply.github.com>
2022-06-10 13:44:19 -0300

    Exercicio 52

--- Exercício52/52.c
+++ Exercício52/52.c
@@ -15,15 +15,46 @@ termos.
 int main(){
 system("cls");
 //DECLARAÇÃO DE VARIAVEIS
+int numero1, numero2, termo, parcial;
 
 //ENTRADA DE VALORES
-
+printf("Insira o primeiro termo da sequencia.\n");
+scanf("%d", &numero1);
+fflush(stdin);
+printf("Insira o segundo termo da sequencia.\n");
+scanf("%d", &numero2);
+fflush(stdin);
+do
+{
+    printf("Insira um numero de termos da sequencia.\n");
+    scanf("%d", &termo);
+    fflush(stdin);
+    if (termo<3){
+        printf("Para existir esta serie sao necessarios pelo menos tres termos.\n");
+    }   
+} while (termo<3);
 
 //CORPO DO ALGORITIMO
-
+printf("%d, %d, ", numero1, numero2);
+for (int i = 2; i < termo; i++)
+{
+    if (i%2==0)
+    {
+        parcial = numero1 + numero2;
+    }else
+    {
+        parcial = numero1 - numero2;
+    }
+    printf("%d", parcial);
+    if(i<(termo-1)){
+        printf(", ");
+    }
+    numero1 = numero2;
+    numero2 = parcial;
+}
 
 //SAIDA DE VALORES
-printf("\n");
+printf("...\n");
 //ENCERRAMENTO DO ALGORITIMO
 system("PAUSE");
 return 0;
