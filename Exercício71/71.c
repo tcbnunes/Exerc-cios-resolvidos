37f8f49046d0d9a1c1790ff0d61675b901677b5e
tcbnunes <106353741+tcbnunes@users.noreply.github.com>
2022-07-05 11:59:47 -0300

    71
    71

--- Exercício71/71.c
+++ Exercício71/71.c
@@ -9,14 +9,32 @@ valor não ocorra no vetor (Busca Sequencial).
 int main(){
 system("cls");
 //DECLARAÇÃO DE VARIAVEIS
-
+int vet [10];
+int valor;
+int verifica = -1;
 //ENTRADA DE VALORES
-
+printf("Insira os 10 valores do vetor\n");
+for (int i = 0; i < 10; i++){
+    scanf("%d", &vet[i]);
+}
+printf("Insira um valor para verificar se ele ocorre.\n");
+scanf ("%d", &valor);
 
 //CORPO DO ALGORITIMO
-
+for (int i = 0; i < 10; i++){
+    if (valor==vet[i]){
+        verifica = i+1;
+    }
+}
 
 //SAIDA DE VALORES
+if (verifica==-1)
+{
+    printf("-1\n");
+}else{
+    printf("O numero ocorre na posicao %d do vetor\n", verifica);
+}
+
 printf("\n");
 //ENCERRAMENTO DO ALGORITIMO
 system("PAUSE");
